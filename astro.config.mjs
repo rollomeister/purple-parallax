0e2ddb54be26561d0ec53b5698e7e826bc865290
Rollomeister <erpeetsalu@gmail.com>
2026-04-21 15:47:12 +0300

    Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://rollomeister.github.io',
+	base: '/purple-parallax',
 	vite: {
 		resolve: {
 			alias: {
